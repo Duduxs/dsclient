fb5a780a177d8a98e4f18437b8d88e90536c95e7
Duduxs <duduxss3@gmail.com>
2020-10-06 10:05:22 -0300

    FindById, ResourceNotFound, StandardError and ErrorController implemented

--- ds-client/src/main/java/com/edudev/dsclient/services/ClientService.java
+++ ds-client/src/main/java/com/edudev/dsclient/services/ClientService.java
@@ -1,14 +1,16 @@
 package com.edudev.dsclient.services;
 
-import javax.transaction.Transactional;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edudev.dsclient.entities.Client;
 import com.edudev.dsclient.repositories.ClientRepository;
+import com.edudev.dsclient.services.exceptions.ResourceNotFound;
 
 @Service
 public class ClientService {
@@ -16,9 +18,15 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public Page<Client> findAllPagination(PageRequest pageRequest){
 		Page<Client> list = repository.findAll(pageRequest);
 		return list;
+	}
+	
+	@Transactional(readOnly = true)
+	public Client findById(Long id){
+		Optional<Client> client = repository.findById(id);
+		return client.orElseThrow(() -> new ResourceNotFound("Resource not founded!"));
 	}
 }
